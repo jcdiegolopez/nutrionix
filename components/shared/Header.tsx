843e62df20e5a8288025215d4629198d80a31392
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2024-05-07 15:09:14 -0600

    logo config

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -4,14 +4,19 @@ import React from 'react'
 import { Button } from '../ui/button'
 import NavItems from './NavItems'
 import MobileNav from './MobileNav'
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({weight: ["100","200","700"], subsets: ['latin']})
 
 export const Header = () => {
   return (
-    <header className='w-full border-b '>
+    <header className='w-full border-b'>
         <div className='wrapper flex items-center justify-between'>
-            <Link href={`/`} className='w-12'>
-                <Image src="/assets/images/logo.png" alt='Nutrionix logo' width={128} height={38} />
-                
+            <Link href={`/`} className='w-10'>
+                <div className='flex flex-row items-center flex-nowrap'>
+                  <Image src="/assets/images/logo.png" alt='Nutrionix logo' width={128} height={38} />
+                  <h3 className={`h3-bold`}>utrionix</h3>
+                </div>
             </Link>
               <nav className='md:flex-between hidden w-full max-w-xs'>
                 <NavItems/>
