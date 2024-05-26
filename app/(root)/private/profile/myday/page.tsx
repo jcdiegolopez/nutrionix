e21863301bd172f1eddb6fce81250a4f4e7e3d03
Diego Lopez <111710248+jcdiegolopez@users.noreply.github.com>
2024-05-26 16:49:39 -0600

    cambios en my profile

--- app/(root)/private/profile/myday/page.tsx
+++ app/(root)/private/profile/myday/page.tsx
@@ -5,9 +5,11 @@ import MyFoods from "@/components/shared/MyFoods";
 import { UserCa } from "@/types";
 import useRouter from "next/navigation";
 
+type PageProps = {
+  doReload?: boolean;
+};
 
-
-const Page = async ({doReload = false} = {}) => {
+const Page: React.FC<PageProps> = async ({doReload = false} = {}) => {
   const user = (await auth())?.user as UserCa;
   console.log(user);
   const isUserDataIncomplete = !user?.gender || !user?.name || !user?.weight || !user?.age || !user?.height || !user?.objective || !user?.activity;
